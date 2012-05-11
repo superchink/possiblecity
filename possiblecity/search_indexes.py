ba80ae5b82573d47ee9930b49eac3a1747124233
Douglas Meehan <dmeehan@gmail.com>
2012-05-11 11:21:32 -0400

    fixing search

--- possiblecity/search_indexes.py
+++ possiblecity/search_indexes.py
@@ -1,15 +1,15 @@
 from haystack.indexes import *
 from haystack import site
-from blog.models import Post
+from blog.models import Entry
 
-class PostIndex(SearchIndex):
+class EntryIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
     author = CharField(model_attr='author')
     pub_date = DateTimeField(model_attr='date_published')
 
     def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return Post.objects.live()
+        return Entry.objects.live()
 
 
-site.register(Post, PostIndex)
+site.register(Entry, EntryIndex)
